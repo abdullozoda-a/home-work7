4d26680b8fd99a0e3a5df85362cbde32e0eebf1c
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-12-04 18:59:31 +0500

    create

---
+++ home work8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace home_work8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            for (int i = a; i > b; i--)
+            {
+                Console.WriteLine(i); 
+            }
+        }
+    }
+}
